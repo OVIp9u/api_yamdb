d83ce5abe7b67349ba06722f4250fa6a8c218c1d
Ивановский Александр <aivanovsky2000@yandex.ru>
2023-04-11 23:32:28 +0300

    Добавлен permissions.py

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -2,9 +2,23 @@ from django.db import models
 
 from django.contrib.auth.models import AbstractUser
 
+ROLES = (
+    ('anonymous', 'Аноним'),
+    ('u')
+)
 
 class User(AbstractUser):
-    bio = models.TextField(
-        'Биография',
-        blank=True,
+    
+    class Role(models.TextChoices):
+        USER = 'user'
+        MODERATOR = 'moderator'
+        ADMIN = 'admin'
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.USER
     )
+
+    def __str__(self):
+        return self.username
