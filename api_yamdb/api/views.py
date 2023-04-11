3fe98115af9bb7620b415f677e41ef3315d3f88e
Андрей Палагута <palagutaandrei@yandex.ru>
2023-04-11 18:46:02 +0300

    Добавлены вьюсеты и сериализаторы комментариев, отзывов и рейтинга, readme

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
+from reviews.models import Review, Comment, Rating
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .serializers import CommentSerializer, ReviewSerializer, RatingSerializer
+from rest_framework import viewsets
+from django.db.models import Avg
 
-# Create your views here.
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    """Вьюсет отзыва"""
+    serializer_class = ReviewSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get_queryset(self):
+        """Метод выбора отзыва по произведению"""
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        """Метод создания отзыва"""
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        serializer.save(author=self.request.user, title=title)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """Вьюсет комментариев"""
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get_queryset(self):
+        """Метод выбора комментариев по отзыву"""
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, pk=review_id)
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        """Метод создания комментария"""
+        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+        serializer.save(author=self.request.user, review=review)
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    """Вьюсет рейтинга"""
+    serializer_class = RatingSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get_queryset(self):
+        """Метод выбора рейтинга по произведению"""
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        rating = title.annotate(score = Avg('reviews__score'))
+        return rating
